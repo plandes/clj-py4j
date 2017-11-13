ace47e42e598815ddd0442cc73d6b0d6787e7ce1
Paul Landes <landes@mailc.net>
2017-11-13 00:47:03 -0600

    fix tests--python3 convert complete

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zensols.clojure",
-    packages = ['zensols.clojure'],
+    packages = ['zensols'],
     version = '0.1',
     description = 'Python to Clojure Bridge using a Py4J Gateway',
     author = 'Paul Landes',
