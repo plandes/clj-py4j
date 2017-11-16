3628d342e241868fe94172785119825deac8cf5d
Paul Landes <landes@mailc.net>
2017-11-16 11:13:28 -0600

    bump py version

--- src/python/setup.py
+++ src/python/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name = "zensols.clojure",
     packages = ['zensols'],
-    version = '0.2',
+    version = '0.3',
     description = 'Python to Clojure Bridge using a Py4J Gateway',
     author = 'Paul Landes',
     author_email = 'landes@mailc.net',
     url = 'https://github.com/plandes/clj-py4j',
-    download_url = 'https://github.com/plandes/clj-py4j/releases/download/v0.0.2/zensols.clojure-0.2-py3-none-any.whl',
+    download_url = 'https://github.com/plandes/clj-py4j/releases/download/v0.0.3/zensols.clojure-0.3-py3-none-any.whl',
     keywords = ['clojure', 'py4j', 'gateway'],
     classifiers = [],
 )
